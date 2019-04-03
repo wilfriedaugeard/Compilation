7db11a486d47c72be2f6af3472f79c0cd8a2349d
Chaima Tarmil <ctarmil@etu.u-bordeaux.fr>
2019-04-03 13:34:31 +0200

    ajout build et main du prof

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,35 @@
+package ubordeaux.deptinfo.compilation.project.main;
 
-import java.io.*;
+import java.io.FileReader;
 
-import abstractTree.AbstTree;
-import abstractTree.AbstTreeInt;
-import abstractTree.Environment;
-import abstractTree.EnvironmentInt;
+import ubordeaux.deptinfo.compilation.project.node.Node;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		ScannerExpr input = new ScannerExpr(new FileReader(args[0]));
-		ParserExpr parser = new ParserExpr();
-		
-		// Le "%goal" du parser doit être un objet de type AbstTreeInt
-		AbstTreeInt expr = (AbstTree) parser.parse(input);
+	private static boolean checksType;
 
-		// On doit pouvoir l'afficher
-		System.out.println(expr.toString());
-		
+	public static void main(String[] args) throws Exception {
+		for (String arg : args) {
+			if (arg.charAt(0) == '-') {
+				if (arg.equals("-checkType"))
+					checksType = true;
+			} else {
+				Scanner input = new Scanner(new FileReader(arg));
+				Parser parser = new Parser();
+				try {
+					System.err.println("*** Fichier " + arg);
+					Node result = (Node) parser.parse(input);
+					System.err.println("*** Analyse syntaxique ok");
+					if (checksType) {
+						if (!result.checksType())
+							System.err.println("*** Erreur de typage");
+						else
+							System.err.println("*** Typage correct");
+					}
+				} catch (beaver.Parser.Exception e) {
+					System.err.println("*** Erreur de syntaxe: " + arg + ":" + e.getMessage());
+				}
+			}
+
+		}
 	}
 }
